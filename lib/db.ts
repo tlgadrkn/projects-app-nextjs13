26d57b029fd4d22666b8ff9e46037cc073964ce4
Tolga Durukan <tolgadurukan16@gmail.com>
2023-03-24 16:27:24 +0200

    init app config

---
+++ lib/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+// Because next API functions run in a serverless environment, this code caches the Prisma Client and reuse it when possible to avoid having too many connections
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
